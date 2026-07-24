6124ae544c41edd4125fa74aed17526bf7bc54bb
ReadyProgramReen <maureenekeugo1@gmail.com>
2026-07-24 14:43:15 -0400

    Add OIDC provider and IAM role/policy for AWS Load Balancer Controller

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -110,3 +110,65 @@ resource "aws_eks_node_group" "main" {
     Name = "${var.project_name}-node-group"
   }
 }
+
+# ---------- OIDC Provider for IRSA ----------
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+  tags = {
+    Name = "${var.project_name}-eks-oidc"
+  }
+}
+
+
+# ---------- AWS Load Balancer Controller IAM Policy ----------
+resource "aws_iam_policy" "alb_controller" {
+  name        = "${var.project_name}-alb-controller-policy"
+  description = "IAM policy for AWS Load Balancer Controller"
+  policy      = data.http.alb_controller_policy.response_body
+}
+
+data "http" "alb_controller_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+# ---------- IAM Role for AWS Load Balancer Controller (IRSA) ----------
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "alb_controller" {
+  name = "${var.project_name}-alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-alb-controller-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
